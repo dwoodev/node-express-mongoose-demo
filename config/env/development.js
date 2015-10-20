9189bf68b84452c968d0f8938acc44d9ca7340cf
Dennis Woo <dwoo@sent.com>
2015-10-20 11:17:58 -0700

    ready for docker dev

--- config/env/development.js
+++ config/env/development.js
@@ -24,7 +24,7 @@ if (fs.existsSync(envFile)) {
  */
 
 module.exports = {
-  db: 'mongodb://localhost/noobjs_dev',
+  	db: 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/noobjs_dev',
   facebook: {
     clientID: process.env.FACEBOOK_CLIENTID,
     clientSecret: process.env.FACEBOOK_SECRET,
